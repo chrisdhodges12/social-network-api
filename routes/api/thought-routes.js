3902ef82b655b6e3b9ee42e8520a9b893ffdf0c3
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-08-20 12:40:57 -0400

    add api routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
 
+const {
+    getAllThoughts,
+    getThoughtById,
+    createNewThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require("../../controllers/thought-controller");
+
+router.route('/').get(getAllThoughts).post(createNewThought);
+
+router.route("/:thoughtId").get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,19 @@
 const router = require("express").Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createNewUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+router.route("/").get(getAllUsers).post(createNewUser);
+
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
